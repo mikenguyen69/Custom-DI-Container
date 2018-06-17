46e5b4c42c23ccdd8365ac39d2fc41ab18421dde
mikenguyen69 <nguyenduchung0609@gmail.com>
2018-06-17 23:11:22 +0800

    Add project files.

---
+++ SimpleDI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
